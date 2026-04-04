3cfd7db842f328afe059d9613c450a02ad4ff6d9
thanugowda10 <thanushreet778@gmail.com>
2026-04-04 16:34:20 +0530

    adding

---
+++ LaptopRunner - Copy.java
@@ -0,0 +1,13 @@
+class LaptopRunner{
+public Static void main(String[] args){
+
+Laptop.getLaptopNamequality("Excellent");
+Laptop.getQuality("Moderate");
+
+int price = Laptop.getPrice();
+System.out.println(price);
+
+int amt = Laptop.getAmt(70, 80);
+System.out.println(amt);
+}
+}
